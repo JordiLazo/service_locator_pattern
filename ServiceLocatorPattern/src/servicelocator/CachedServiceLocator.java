e65a93f1b6dfdde5f8eff06a81701ae747fa1489
Jordi Lazo <jordilazo94@gmail.com>
2021-05-20 21:33:08 +0200

    delete commets

--- ServiceLocatorPattern/src/servicelocator/CachedServiceLocator.java
+++ ServiceLocatorPattern/src/servicelocator/CachedServiceLocator.java
@@ -25,7 +25,6 @@ public class CachedServiceLocator implements ServiceLocator {
     public void setService(String name, Factory factory) throws LocatorError {
         if (!dictionary.containsKey(name)){
             dictionary.put(name,new Object[]{factory,null});
-            //System.err.println("Added Service with name="+name);
         }else {
             throw new LocatorError("ALready Factory registered to '"+name+"'");
         }
@@ -35,7 +34,6 @@ public class CachedServiceLocator implements ServiceLocator {
     public void setConstant(String name, Object value) throws LocatorError {
         if (!dictionary.containsKey(name)) {
             dictionary.put(name,new Object[]{null,value});
-            //System.err.println("Added implemntatiom with name="+name);
         }else{
             throw new LocatorError("A constant is registered to '"+name+"'");
         }
@@ -43,18 +41,11 @@ public class CachedServiceLocator implements ServiceLocator {
 
     @Override
     public Object getObject(String name) throws LocatorError {
-        // Nom-Factoria-constant=null--> Factoria.create
-        // Nom Factoria-constant ---> constant
-
         if (dictionary.containsKey(name)) {
             Object[] elements = dictionary.get(name);
-            //0 -> factory
-            //1-> Implemntation
             if (elements[1] != null) {
-                //System.err.println("Returning cached implementation named: "+name);
                 return elements[1];
             }else{
-                //System.err.println("Not found an implementation named: "+name+" creating it");
                 Factory factory = (Factory) elements[0];
                 elements[1] =factory.create(this);
 
--- ServiceLocatorPattern/src/servicelocator/SimpleServiceLocator.java
+++ ServiceLocatorPattern/src/servicelocator/SimpleServiceLocator.java
@@ -24,7 +24,6 @@ public class SimpleServiceLocator implements ServiceLocator{
     public void setService(String name, Factory factory) throws LocatorError {
         if (!dictionary.containsKey(name)){
             dictionary.put(name,new Object[]{factory,null});
-            //System.err.println("Added Service with name="+name);
         }else {
             throw new LocatorError("ALready Factory registered to '"+name+"'");
         }
@@ -34,7 +33,6 @@ public class SimpleServiceLocator implements ServiceLocator{
     public void setConstant(String name, Object value) throws LocatorError {
         if (!dictionary.containsKey(name)) {
             dictionary.put(name,new Object[]{null,value});
-            //System.err.println("Added implemntatiom with name="+name);
         }else{
             throw new LocatorError("A constant is registered to '"+name+"'");
         }
@@ -44,13 +42,9 @@ public class SimpleServiceLocator implements ServiceLocator{
     public Object getObject(String name) throws LocatorError {
         if (dictionary.containsKey(name)) {
             Object[] elements = dictionary.get(name);
-            //0 -> factory
-            //1-> Implemntation
             if (elements[1] != null) {
-                //System.err.println("Returning cached implementation named: "+name);
                 return elements[1];
             }else{
-                //System.err.println("Not found an implementation named: "+name+" creating it");
                 Factory factory = (Factory) elements[0];
                 return factory.create(this);
             }
