f160e71126d849af732ae56a8eba3d684f9de132
Flor Martinez <floormartinez.fmm@gmail.com>
2021-05-20 15:01:38 +0200

    all test done

--- ServiceLocatorPattern/Test/servicelocator2/CachedServiceLocatortest.java
+++ ServiceLocatorPattern/Test/servicelocator2/CachedServiceLocatortest.java
@@ -5,6 +5,7 @@ import mocks.A.FactoryA1v2;
 import mocks.B.FactoryB1v2;
 import mocks.B.ImplementationB1;
 import mocks.B.InterfaceB;
+import mocks.C.FactoryC1v2;
 import mocks.C.ImplementationC1;
 import mocks.C.InterfaceC;
 import mocks.D.FactoryD1v2;
@@ -13,8 +14,7 @@ import mocks.D.InterfaceD;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CachedServiceLocatortest {
 
@@ -46,5 +46,41 @@ class CachedServiceLocatortest {
             csl.setConstant(Object.class, o);
             csl.setConstant(Object.class, o1);
         });
+    }
+    //FactoryB1v2
+    @Test
+    <T> void setServiceTestB1() throws LocatorError {
+
+        csl.setConstant(InterfaceD.class, new ImplementationD1(20));
+        csl.setService(FactoryB1v2.class, new FactoryB1v2());
+
+        assertSame(csl.getObject(FactoryB1v2.class), csl.getObject(FactoryB1v2.class));
+
+        T string = (T) "prova";
+        Class constantClass = String.class;
+
+        csl.setConstant(constantClass, string);
+        csl.setService(FactoryC1v2.class, new FactoryC1v2());
+        assertSame(csl.getObject(FactoryC1v2.class), csl.getObject(FactoryC1v2.class));
+
+    }
+    //FactoryC1v2
+    @Test
+    <T> void setServiceTestC1() throws LocatorError {
+
+        T string = (T) "prova";
+        Class constantClass = String.class;
+
+        csl.setConstant(constantClass, string);
+        csl.setService(FactoryC1v2.class, new FactoryC1v2());
+
+        assertSame(csl.getObject(FactoryC1v2.class), csl.getObject(FactoryC1v2.class));
+
+        Integer integer = 20;
+
+        csl.setConstant(Integer.class, integer);
+        csl.setService(FactoryD1v2.class, new FactoryD1v2());
+        assertSame(csl.getObject(FactoryD1v2.class), csl.getObject(FactoryD1v2.class));
+
     }
 }
